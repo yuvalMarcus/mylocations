6f18b4d178a133fe4d0196186ad8cc513810c45d
yuvalMarcus <yuval.marcus.90@gmail.com>
2021-05-29 23:55:02 +0300

    update project

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <footer className={'flex space-x-4 bg-gray-700 text-white p-2'}>
             <Link className={'bg-gray-500 text-white rounded p-1 px-2 hover:bg-gray-600'} to="/locations">
-                locations
+                Locations
             </Link>
             <Link className={'bg-gray-500 text-white rounded p-1 px-2 hover:bg-gray-600'} to="/categories">
                 Categories
